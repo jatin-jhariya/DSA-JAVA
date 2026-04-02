d2492cdd8ab35b5bcfabb95655d88b3d70de345a
jatin-jhariya <jatinjhariya0710@gmail.com>
2026-04-02 10:07:57 +0530

    function/method problems

---
+++ lect1.java
@@ -0,0 +1,103 @@
+package _functions_methods;
+// import java.util.*;
+
+public class lect1 {
+
+    // public static void printHW(){
+    //     System.out.println("hello world");
+    //     return;
+    // }
+
+    // public static int Sum(int num1,int num2)  // parameters or formal parameters
+    // {
+    //     int sum= num1+ num2;
+    //     return sum;
+    // }
+
+    // public static void changeA(int a){
+    //     a=10;
+    //     System.out.print(a); 
+    // }
+
+    // public static int multiply(int a, int b){
+    //     return a*b; 
+    // }
+
+    // factorial
+    // public static int isFact(int num){
+    //     int fact=1;
+    //     for(int i=1; i<=num ; i++){
+    //     fact= i*fact;
+        
+    // }
+    //     return fact;
+    // }
+
+    // public static int BinC(int n, int r){
+    //     float binCoeff;
+    //     float a= isFact(n);
+    //     float b= isFact(r);
+    //     float c= isFact(n-r);
+    //     binCoeff= a/(b*c);
+    //     return (int)binCoeff;
+    // }
+
+    // function overloading
+    // public static int sum(int a, int b){
+    //     return a+b;
+    // }
+    // public static int sum(int a, int b, int c){
+    //     return a+b+c;
+    // }
+
+    // to cal sum of two num
+    // public static int sum(int a, int b){
+    //     return a+b;
+    // }
+
+    // public static float sum(float a, float b){
+    //     return a+b;
+    // }
+
+    public static void main(String args[]){
+    //    printHW();
+
+    // sum of two numbers
+    // Scanner sc= new Scanner(System.in);
+    // System.out.print("enter 1st number: ");
+    // int a=sc.nextInt();
+    // System.out.print("enter 2nd number: ");
+    // int b=sc.nextInt();
+    // int sum=Sum(a,b);  // arguments or actual parameters
+    // System.out.print("Sum is: "+sum);
+    // sc.close();
+
+    // int a=5;
+    // changeA(a);
+    
+
+    // product of two numbers 
+    // int a=3;
+    // int b=4;
+    // int product= multiply(a,b);
+    // System.out.print("a*b = "+product);
+
+    // binomial coefficient
+    // System.out.print("enter n: ");
+    // Scanner sc= new Scanner(System.in);
+    // int n=sc.nextInt();
+    // System.out.print("enter r: ");
+    // int r=sc.nextInt();
+
+    // System.out.print("Binomial coefficient is: "+BinC(n, r));
+    // sc.close();
+
+    // sum of two numbers
+    // System.out.print("Sum is: "+sum(3,4));
+
+    // sum of two num 
+    // System.out.println(sum(3,2));
+    // System.out.print(sum(3.2f,1.8f));
+    
+    }
+}
